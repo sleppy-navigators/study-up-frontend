06689872a6c3c5b1f0e9cffabe251836eb359a14
Byeolchan Kim <qufstar46@gmail.com>
2025-02-09 04:33:50 +0900

    infra: set up react query

---
+++ lib/react-query.tsx
@@ -0,0 +1,40 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DevToolsBubble } from 'react-native-react-query-devtools';
+import { ReactNode } from 'react';
+import * as Clipboard from 'expo-clipboard';
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        gcTime: 1000 * 60 * 60 * 24, // 24 hours
+        retry: 3,
+        refetchOnReconnect: true,
+      },
+    },
+  });
+
+  const onCopy = async (text: string) => {
+    try {
+      await Clipboard.setStringAsync(text);
+
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {process.env.NODE_ENV === 'development' && (
+        <DevToolsBubble onCopy={onCopy} />
+      )}
+    </QueryClientProvider>
+  );
+}
