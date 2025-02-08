416600f957f582f4836ac231ae76fc47f9ec196b
Byeolchan Kim <qufstar46@gmail.com>
2025-02-09 04:30:06 +0900

    feat: add required dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,28 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Providers } from '@/lib/react-query';
+import { ActivityIndicator, View } from 'react-native';
+import * as encoding from 'text-encoding';
+
+const LoadingScreen = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <ActivityIndicator size="large" />
+  </View>
+);
 
 export default function RootLayout() {
   return (
-    <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-        <Stack.Screen name="login" />
-      </Stack>
-      <StatusBar style="light" />
-    </>
+    <Providers>
+      <Suspense fallback={<LoadingScreen />}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen name="login" />
+          <Stack.Screen name="chat" />
+        </Stack>
+        <StatusBar style="light" />
+      </Suspense>
+    </Providers>
   );
 }
