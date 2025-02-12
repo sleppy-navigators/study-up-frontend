06c6453765105bcce3a65660b9a61f852058eb6a
Byeolchan Kim <qufstar46@gmail.com>
2025-02-13 00:42:55 +0900

    chore: remove extra comments

--- hooks/useGoogleSignIn.ts
+++ hooks/useGoogleSignIn.ts
@@ -6,10 +6,8 @@ import { signInWithGoogle } from '../lib/firebase';
 export const useGoogleSignIn = () => {
   const signIn = async () => {
     try {
-      // Google 로그인 및 ID 토큰 획득
       const idToken = await signInWithGoogle();
 
-      // 백엔드로 ID 토큰 전송
       const response = await client
         .post('auth/sign-in', {
           searchParams: {
@@ -28,10 +26,7 @@ export const useGoogleSignIn = () => {
         }>();
 
       if (response.apiResult === 'QUERY_OK') {
-        // 토큰 저장 및 상태 업데이트
         await authActions.setTokens(response.data);
-
-        // 채팅 화면으로 리다이렉트
         router.replace('/chat');
       }
     } catch (error) {
