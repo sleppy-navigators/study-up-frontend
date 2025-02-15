8ea6d41428f31159ff929065b4d3a0b23d9c61bb
Byeolchan Kim <qufstar46@gmail.com>
2025-02-15 22:22:06 +0900

    refactor: remove ErrorProvider and SuspenseProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,24 +4,18 @@ import React from 'react';
 import { Providers } from '@/lib/react-query';
 import { TamaguiProvider } from 'tamagui';
 import appConfig from '@/tamagui.config';
-import { ErrorBoundaryProvider } from '@/components/ErrorBoundaryProvider';
-import { SuspenseProvider } from '@/components/SuspenseProvider';
 
 export default function RootLayout() {
   return (
     <TamaguiProvider config={appConfig}>
       <Providers>
-        <ErrorBoundaryProvider>
-          <SuspenseProvider>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="+not-found" />
-              <Stack.Screen name="login" />
-              <Stack.Screen name="chat" />
-            </Stack>
-            <StatusBar style="light" />
-          </SuspenseProvider>
-        </ErrorBoundaryProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen name="login" />
+          <Stack.Screen name="chat" />
+        </Stack>
+        <StatusBar style="light" />
       </Providers>
     </TamaguiProvider>
   );
