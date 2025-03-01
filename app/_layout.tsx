cd42e3003f5841729bc0e73bceaf67d07103c182
Byeolchan Kim <qufstar46@gmail.com>
2025-03-02 06:51:43 +0900

    feat: add SafeAreaProvider to RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,26 +11,29 @@ import {
   ThemeProvider,
 } from '@react-navigation/native';
 import { Header } from '@/base/components/header';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return (
     <TamaguiProvider config={appConfig} defaultTheme={colorScheme!}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <QueryProvider>
-          <Stack
-            screenOptions={{
-              header: (props) => <Header />,
-            }}>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-            <Stack.Screen name="login" />
-            <Stack.Screen name="chat" />
-            <Stack.Screen name="bounty" options={{ headerShown: true }} />
-            <Stack.Screen name="group" options={{ headerShown: true }} />
-          </Stack>
-          <StatusBar style="light" />
-        </QueryProvider>
+        <SafeAreaProvider>
+          <QueryProvider>
+            <Stack
+              screenOptions={{
+                header: (props) => <Header />,
+              }}>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="+not-found" />
+              <Stack.Screen name="login" />
+              <Stack.Screen name="chat" />
+              <Stack.Screen name="bounty" options={{ headerShown: true }} />
+              <Stack.Screen name="group" options={{ headerShown: true }} />
+            </Stack>
+            <StatusBar style="light" />
+          </QueryProvider>
+        </SafeAreaProvider>
       </ThemeProvider>
     </TamaguiProvider>
   );
