f2e41d2091a46cf8c0141fbb6a09820242063e25
Byeolchan Kim <qufstar46@gmail.com>
2025-04-05 20:37:02 +0900

    refactor: use logout from zustand store

--- hooks/useRecoverFromError.ts
+++ hooks/useRecoverFromError.ts
@@ -1,12 +1,12 @@
 import { useQueryClient } from '@tanstack/react-query';
-import { router } from 'expo-router';
-import { useSetAtom } from 'jotai';
-import { authActions } from '@/lib/auth/authStore';
+import { router, usePathname } from 'expo-router';
 import { useCallback } from 'react';
 import { ResetOptions } from '@/lib/errors/http';
+import { useAuthStore } from '@/auth/stores/authStore';
 
 export const useRecoverFromError = () => {
   const queryClient = useQueryClient();
+  const logout = useAuthStore((state) => state.logout);
 
   const recoverFromError = useCallback(
     async (resetOptions: ResetOptions) => {
@@ -17,19 +17,17 @@ export const useRecoverFromError = () => {
         navigationTarget = '/',
       } = resetOptions;
 
-      if (shouldClearCache) {
-        queryClient.clear();
-      }
+      if (shouldClearCache) queryClient.clear();
 
       if (shouldClearAuth) {
-        await authActions.clearTokens();
+        logout();
       }
 
       if (shouldClearNavigation) {
         router.replace(navigationTarget);
       }
     },
-    [queryClient]
+    [queryClient, logout]
   );
 
   return { recoverFromError };
