c9e56cb819061ba3beb42d601cfe57e1104a763c
Byeolchan Kim <qufstar46@gmail.com>
2025-05-08 14:06:08 +0900

    storybook: decorate stories with providers globally

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,10 @@
 import type { Preview } from '@storybook/react';
 import React from 'react';
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { TamaguiProvider } from 'tamagui';
 import appConfig from '../tamagui.config';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { QueryProvider } from '../lib/react-query';
 
 const preview: Preview = {
   parameters: {
@@ -14,8 +17,16 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
-      <TamaguiProvider config={appConfig} defaultTheme="light">
-        <Story />
+      <TamaguiProvider config={appConfig} defaultTheme={'light'}>
+        <ThemeProvider value={DefaultTheme}>
+          <SafeAreaProvider>
+            <QueryProvider>
+              <SafeAreaView style={{ flex: 1 }}>
+                <Story />
+              </SafeAreaView>
+            </QueryProvider>
+          </SafeAreaProvider>
+        </ThemeProvider>
       </TamaguiProvider>
     ),
   ],
