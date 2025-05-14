10318aa9466c601602d3c5ecab2b2211a6d0607e
Byeolchan Kim <qufstar46@gmail.com>
2025-05-15 01:09:59 +0900

    feat: add typography components and adjust their default sizes

---
+++ base/components/heading.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  styled,
+  H1 as TamaguiH1,
+  H2 as TamaguiH2,
+  H3 as TamaguiH3,
+  H4 as TamaguiH4,
+  H5 as TamaguiH5,
+  H6 as TamaguiH6,
+} from 'tamagui';
+
+export const Heading1 = styled(TamaguiH1, {
+  size: '$8',
+});
+
+export const Heading2 = styled(TamaguiH2, {
+  size: '$7',
+});
+
+export const Heading3 = styled(TamaguiH3, {
+  size: '$6',
+});
+
+export const Heading4 = styled(TamaguiH4, {
+  size: '$5',
+});
+
+export const Heading5 = styled(TamaguiH5, {
+  size: '$4',
+});
+
+export const Heading6 = styled(TamaguiH6, {
+  size: '$3',
+});
---
+++ base/components/paragraph.tsx
@@ -0,0 +1,5 @@
+import { styled, Paragraph as TamaguiParagraph } from 'tamagui';
+
+export const Paragraph = styled(TamaguiParagraph, {
+  size: '$4',
+});
