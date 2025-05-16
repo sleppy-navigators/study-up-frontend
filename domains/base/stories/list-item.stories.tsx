0f6ca9c95f94c184e142d5e95b59b18d78dc4334
Byeolchan Kim <qufstar46@gmail.com>
2025-05-17 01:46:50 +0900

    refactor: move to correct directory

--- domains/base/stories/list-item.stories.tsx
+++ domains/base/stories/list-item.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { ListItem } from './list-item';
+import { ListItem } from '../components/list-item';
 
 const meta = {
   title: 'Base/Components/ListItem',
