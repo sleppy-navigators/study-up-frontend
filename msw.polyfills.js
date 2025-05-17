a37afbd144e27c1a995171202b73628f89db8a77
Byeolchan Kim <qufstar46@gmail.com>
2025-05-18 03:20:20 +0900

    feat: polyfill for react native ecosystem

---
+++ msw.polyfills.js
@@ -0,0 +1,17 @@
+import 'fast-text-encoding';
+import 'react-native-url-polyfill/auto';
+
+function defineMockGlobal(name) {
+  if (typeof global[name] === 'undefined') {
+    global[name] = class {
+      constructor(type, eventInitDict) {
+        this.type = type;
+        Object.assign(this, eventInitDict);
+      }
+    };
+  }
+}
+
+['MessageEvent', 'Event', 'EventTarget', 'BroadcastChannel'].forEach(
+  defineMockGlobal
+);
