45f194fa4e5a900a90da14146510987213965558
Byeolchan Kim <qufstar46@gmail.com>
2025-05-25 03:02:48 +0900

    feat: add login

--- app/(fallback)/index.tsx
+++ app/(fallback)/index.tsx
@@ -1,14 +1,16 @@
 import { Link } from 'expo-router';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View>
-      <Link href="/login">Login</Link>
-      <Link href="/chat">Chat</Link>
+      <Link href="/login">
+        <Text>Login</Text>
+      </Link>
+      {/*<Link href="/chat">Chat</Link>
       <Link href="/bounty">Bounty</Link>
       <Link href="/group">Group</Link>
-      <Link href="/group/create">Group Create</Link>
+      <Link href="/group/create">Group Create</Link> */}
     </View>
   );
 }
