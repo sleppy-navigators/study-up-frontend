37b5b69dbfed1402d8ccb6a3ff2724d8b5981970
Byeolchan Kim <qufstar46@gmail.com>
2025-05-31 16:55:25 +0900

    feat: remove collection enabled settings

--- domains/base/providers/crashlytics-provider.tsx
+++ domains/base/providers/crashlytics-provider.tsx
@@ -4,8 +4,6 @@ import crashlytics from '@react-native-firebase/crashlytics';
 
 async function initializeCrashlytics() {
   console.log('Crashlytics is initializing...');
-  await crashlytics().setCrashlyticsCollectionEnabled(true);
-  console.log('Crashlytics has been initialized.');
   crashlytics().crash();
   console.log('Crashlytics has crashed for testing.');
 }
