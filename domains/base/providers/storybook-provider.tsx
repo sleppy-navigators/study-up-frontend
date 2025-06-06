e38a5082697566bb2498288cf24e2c65414ad39b
Byeolchan Kim <qufstar46@gmail.com>
2025-06-06 19:50:43 +0900

    fix: avoid storybook top level import when disabled

--- domains/base/providers/storybook-provider.tsx
+++ domains/base/providers/storybook-provider.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Constants from 'expo-constants';
-import StorybookUIRoot from '../../../.storybook';
 
-export default function StorybookProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const isStorybookEnabled = Constants.expoConfig?.extra?.storybookEnabled;
+const isStorybookEnabled = Constants.expoConfig?.extra?.storybookEnabled;
 
-  if (isStorybookEnabled) {
-    return <StorybookUIRoot />;
-  }
-
+function App({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
+
+let AppEntryPoint = App;
+
+if (isStorybookEnabled) {
+  const { default: StorybookUIRoot } = require('@/.storybook');
+  AppEntryPoint = StorybookUIRoot;
+}
+
+export default AppEntryPoint;
